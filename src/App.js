06649b9483985d88ecca463d20e5a7d7ddfa8a33
NagarjunC4E <nagarjuna@care4edu.com>
2024-12-03 20:51:59 +0530

    content changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import styled, { keyframes, createGlobalStyle } from 'styled-components';
 import {
   Heart as HeartIcon,
